79e144be4301d5128475577d5688c5e15bdc27bb
gzy <283211498@qq.com>
2019-02-02 23:06:06 +0800

    完善 iconButton 使 footer 的左右箭头实现功能

--- js/script.js
+++ js/script.js
@@ -2,30 +2,70 @@ var Footer = {
     init: function () {
         this.$footer = $('footer');
         this.$ul = this.$footer.find('ul')
+        this.$box = this.$footer.find('.box')
+        this.$leftButton = this.$footer.find('.icon-arrow_left')
+        this.$rightButton = this.$footer.find('.icon-arrow_right')
+        this.isToEnd = false
+        this.isToStart = true
         this.bind();
         this.render()
     },
 
     bind: function () {
         var _this = this
+
         $(window).resize(function () {
-            console.log('ok');
             _this.setStyle()
         })
+
+        this.$rightButton.on('click', function () {
+            var itemWidth = _this.$footer.find('li').outerWidth(true)
+            var rowCount = Math.floor(_this.$box.width() / itemWidth)
+
+            if (!_this.isToEnd) {
+                _this.$ul.animate({
+                    left: "-=" + rowCount * itemWidth
+                }, 400, function () {
+                    _this.isToStart = false
+                    if (_this.$box.width() - parseFloat(_this.$ul.css('left')) >= parseFloat(_this.$ul.css('width'))) {
+                        _this.isToEnd = true
+                        // _this.$rightButton.addClass('disabled')
+                    }
+                })
+            }
+        })
+
+        this.$leftButton.on('click', function () {
+            var itemWidth = _this.$footer.find('li').outerWidth(true)
+            var rowCount = Math.floor(_this.$box.width() / itemWidth)
+
+            if (!_this.isToStart) {
+                _this.$ul.animate({
+                    left: "+=" + rowCount * itemWidth
+                }, 400, function () {
+                    _this.isToEnd = false
+                    if (parseFloat(_this.$ul.css('left')) >= 0 ) {
+                        _this.isToStart = true
+                    }
+                })
+            }
+        })
+
     },
 
-    render: function () {
-        var _this = this;
-        $.getJSON('http://api.jirengu.com/fm/getChannels.php').done(function (ret) {
-            console.log(ret);
-            _this.renderFooter(ret.channels)
-        }).fail(function () {
-            console.log('error');
-        })
-    },
+    render:
+
+        function () {
+            var _this = this;
+            $.getJSON('http://api.jirengu.com/fm/getChannels.php').done(function (ret) {
+                console.log(ret);
+                _this.renderFooter(ret.channels)
+            }).fail(function () {
+                console.log('error');
+            })
+        },
 
     renderFooter: function (channels) {
-        console.log('renderFooter');
         var _this = this;
         channels.forEach(function (channel) {
             template = `
@@ -45,9 +85,7 @@ var Footer = {
         var count = this.$footer.find('ul li').length;
         var width = this.$footer.find('ul li').outerWidth(true)
         this.$ul.css({width: count * width + "px"})
-        console.log(count, width);
     }
-    //刷新的时候正常 resize屏幕后效果消失
 }
 
 
